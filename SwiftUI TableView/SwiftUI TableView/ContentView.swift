ab0f450b3cc9df1da1a2230f6c01cec4d7b43026
Noshaid Ali <noshaid.ali@cricingif.com>
2020-03-22 20:20:09 +0500

    [Start] start working

---
+++ SwiftUI TableView/SwiftUI TableView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI TableView
+//
+//  Created by Noshaid Ali on 22/03/2020.
+//  Copyright © 2020 Noshaid Ali. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI TableView/SwiftUI TableViewTests/SwiftUI_TableViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_TableViewTests.swift
+//  SwiftUI TableViewTests
+//
+//  Created by Noshaid Ali on 22/03/2020.
+//  Copyright © 2020 Noshaid Ali. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_TableView
+
+class SwiftUI_TableViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
