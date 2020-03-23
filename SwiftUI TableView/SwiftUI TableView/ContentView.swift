7cf7c36fb5c26d9368e2e2fcaeb106a45791b8a8
Noshaid Ali <noshaid.ali@cricingif.com>
2020-03-23 21:42:09 +0500

    [Setup] setting up DiffableDataSource

--- SwiftUI TableView/SwiftUI TableView/ContentView.swift
+++ SwiftUI TableView/SwiftUI TableView/ContentView.swift
@@ -8,14 +8,78 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+enum SectionType {
+    case ceo, peasants
+}
+
+struct Contact: Hashable {
+    let name: String
+}
+
+class DiffableTableViewController: UITableViewController {
+    
+    //UITableViewDiffableDataSource
+    
+    lazy var source: UITableViewDiffableDataSource<SectionType, Contact> = .init(tableView: self.tableView) {
+        (tableView, indexPath, conatct) -> UITableViewCell? in
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = conatct.name
+        return cell
     }
+    
+    private func setupSource() {
+        var snapshot = source.snapshot()
+        snapshot.appendSections([.ceo, .peasants])
+        snapshot.appendItems([
+            .init(name: "Elon Musk"),
+            .init(name: "Tim Cook")
+        ], toSection: .ceo)
+        snapshot.appendItems([.init(name: "Bill Gates")], toSection: .peasants)
+        source.apply(snapshot)
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = section == 0 ? "CEO" : "Peasant"
+        return label
+    }
+
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        setupSource()
+    }
+}
+
+struct DiffableContainer: UIViewControllerRepresentable {
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<DiffableContainer>) -> UIViewController {
+        UINavigationController(rootViewController: DiffableTableViewController(style: .insetGrouped))
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: UIViewControllerRepresentableContext<DiffableContainer>) {
+        
+    }
+    
+    typealias UIViewControllerType = UIViewController
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        //ContentView()
+        DiffableContainer()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
     }
 }
